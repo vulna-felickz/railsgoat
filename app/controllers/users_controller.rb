cb570155e32994ce5f251fe2504b6c89d56b835b
Chad Bentz <1760475+felickz@users.noreply.github.com>
2023-02-22 16:42:25 -0500

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,6 +54,29 @@ class UsersController < ApplicationController
       redirect_to user_account_settings_path(user_id: current_user.id)
     end
   end
+  
+  
+  def update2
+    message = false
+
+    user = User.where("id = '#{params[:user][:id]}'")[0]
+
+    if user
+      user.update(user_params_without_password)
+      if params[:user][:password].present? && (params[:user][:password] == params[:user][:password_confirmation])
+        user.password = params[:user][:password]
+      end
+      message = true if user.save!
+      respond_to do |format|
+        format.html { redirect_to user_account_settings_path(user_id: current_user.id) }
+        format.json { render json: {msg: message ? "success" : "false "} }
+      end
+    else
+      flash[:error] = "Could not update user!"
+      redirect_to user_account_settings_path(user_id: current_user.id)
+    end
+  end
+  
 
   private
 
